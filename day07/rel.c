4aa10f1186b3c703279f545ad2b58e0157394ae8
peng <1912129881@qq.com>
2024-03-28 20:10:11 +0800

    关系运算符的学习

---
+++ day07/rel.c
@@ -0,0 +1,14 @@
+//关系运算符
+#include <stdio.h>
+
+int main(void)
+{
+	printf("1 == 1的结果是%d\n", 1 == 1);
+	printf("1 != 1的结果是%d\n", 1 != 1);
+	printf("1 > 1的结果是%d\n", 1 > 1);
+	printf("1 > 1的结果是%d\n", 1 < 1);
+	printf("1 == 1的结果是%d\n", 1 <= 2);
+	printf("1 >= 1的结果是%d\n", 1 >= 2);
+
+	return 0;
+}
